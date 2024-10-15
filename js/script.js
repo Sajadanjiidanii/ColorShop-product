9363d7471643ba37119856631fa975d0308a50eb
Sajadanjiidanii <154419813+Sajadanjiidanii@users.noreply.github.com>
2024-10-15 15:59:00 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,35 +5,35 @@ const bestSellerItem = $.getElementById("bestsellerItem");
 const otherItem = $.getElementById("otherItem");
 
 const categorys = [
-        { categoryName : "Colors", name : "رنگ هنری قهوه ای" , pricy : 120 , img : "imgs/New Products/Colors Products/Product_1.png" },
-        { categoryName : "Colors", name : "رنگ هنری صورتی" , pricy : 120 , img : "imgs/New Products/Colors Products/Product_2.png" },
-        { categoryName : "Colors", name : "رنگ هنری سبز" , pricy : 120 , img : "imgs/New Products/Colors Products/Product_3.png" },
-        { categoryName : "Colors", name : "رنگ هنری آبی" , pricy : 120 , img : "imgs/New Products/Colors Products/Product_4.png" },
+        { categorySearch : "Colors", categoryName : "رنگ" , name : "رنگ هنری قهوه ای" , pricy : 120 , img : "imgs/New Products/Colors Products/Product_1.png" },
+        { categorySearch : "Colors", categoryName : "رنگ" , name : "رنگ هنری صورتی" , pricy : 120 , img : "imgs/New Products/Colors Products/Product_2.png" },
+        { categorySearch : "Colors", categoryName : "رنگ" , name : "رنگ هنری سبز" , pricy : 120 , img : "imgs/New Products/Colors Products/Product_3.png" },
+        { categorySearch : "Colors", categoryName : "رنگ" , name : "رنگ هنری آبی" , pricy : 120 , img : "imgs/New Products/Colors Products/Product_4.png" },
 
-        { categoryName : "Dishes", name : "گلدان بتنی دایره ای" , pricy : 200 , img : "imgs/New Products/Dishes Products/Product_1.png" },
-        { categoryName : "Dishes", name : "گلدان بتنی شیش ضلعی" , pricy : 200 , img : "imgs/New Products/Dishes Products/Product_2.png" },
-        { categoryName : "Dishes", name : "گلدان بتنی شیش ضلعی سه بعدی" , pricy : 200 , img : "imgs/New Products/Dishes Products/Product_3.png" },
-        { categoryName : "Dishes", name : "گلدان بتنی ستاره ای سه بعدی" , pricy : 200 , img : "imgs/New Products/Dishes Products/Product_4.png" },
+        { categorySearch : "Dishes", categoryName : "ظروف خام" , name : "گلدان بتنی دایره ای" , pricy : 200 , img : "imgs/New Products/Dishes Products/Product_1.png" },
+        { categorySearch : "Dishes", categoryName : "ظروف خام" , name : "گلدان بتنی شیش ضلعی" , pricy : 200 , img : "imgs/New Products/Dishes Products/Product_2.png" },
+        { categorySearch : "Dishes", categoryName : "ظروف خام" , name : "گلدان بتنی شیش ضلعی سه بعدی" , pricy : 200 , img : "imgs/New Products/Dishes Products/Product_3.png" },
+        { categorySearch : "Dishes", categoryName : "ظروف خام" , name : "گلدان بتنی ستاره ای سه بعدی" , pricy : 200 , img : "imgs/New Products/Dishes Products/Product_4.png" },
 
-        { categoryName : "Epoxys", name : "جوهر الکلی" , pricy : 190 , img : "imgs/New Products/Epoxys Products/Product_1.png" },
-        { categoryName : "Epoxys", name : "جوهر الکلی" , pricy : 190 , img : "imgs/New Products/Epoxys Products/Product_2.png" },
-        { categoryName : "Epoxys", name : "جوهر الکلی" , pricy : 190 , img : "imgs/New Products/Epoxys Products/Product_3.png" },
-        { categoryName : "Epoxys", name : "جوهر الکلی" , pricy : 190 , img : "imgs/New Products/Epoxys Products/Product_4.png" },
+        { categorySearch : "Epoxys", categoryName : "محصولات اپوکسی" , name : "جوهر الکلی" , pricy : 190 , img : "imgs/New Products/Epoxys Products/Product_1.png" },
+        { categorySearch : "Epoxys", categoryName : "محصولات اپوکسی" , name : "جوهر الکلی" , pricy : 190 , img : "imgs/New Products/Epoxys Products/Product_2.png" },
+        { categorySearch : "Epoxys", categoryName : "محصولات اپوکسی" , name : "جوهر الکلی" , pricy : 190 , img : "imgs/New Products/Epoxys Products/Product_3.png" },
+        { categorySearch : "Epoxys", categoryName : "محصولات اپوکسی" , name : "جوهر الکلی" , pricy : 190 , img : "imgs/New Products/Epoxys Products/Product_4.png" },
  
-        { categoryName : "Sprays", name : "اسپری نارنجی" , pricy : 150 , img : "imgs/New Products/Sprays Products/Product_1.png" },
-        { categoryName : "Sprays", name : "اسپری زرد" , pricy : 150 , img : "imgs/New Products/Sprays Products/Product_2.png" },
-        { categoryName : "Sprays", name : "اسپری قرمز" , pricy : 150 , img : "imgs/New Products/Sprays Products/Product_3.png" },
-        { categoryName : "Sprays", name : "اسپری صورتی" , pricy : 150 , img : "imgs/New Products/Sprays Products/Product_4.png" },
+        { categorySearch : "Sprays", categoryName : "اسپری" , name : "اسپری نارنجی" , pricy : 150 , img : "imgs/New Products/Sprays Products/Product_1.png" },
+        { categorySearch : "Sprays", categoryName : "اسپری" , name : "اسپری زرد" , pricy : 150 , img : "imgs/New Products/Sprays Products/Product_2.png" },
+        { categorySearch : "Sprays", categoryName : "اسپری" , name : "اسپری قرمز" , pricy : 150 , img : "imgs/New Products/Sprays Products/Product_3.png" },
+        { categorySearch : "Sprays", categoryName : "اسپری" , name : "اسپری صورتی" , pricy : 150 , img : "imgs/New Products/Sprays Products/Product_4.png" },
 
-        { categoryName : "Supplies", name : "خمیر شمع" , pricy : 100 , img : "imgs/New Products/Supplies Products/Product_1.png" },
-        { categoryName : "Supplies", name : "خمیر شمع" , pricy : 100 , img : "imgs/New Products/Supplies Products/Product_2.png" },
-        { categoryName : "Supplies", name : "خمیر شمع" , pricy : 100 , img : "imgs/New Products/Supplies Products/Product_3.png" },
-        { categoryName : "Supplies", name : "خمیر شمع" , pricy : 100 , img : "imgs/New Products/Supplies Products/Product_4.png" },
+        { categorySearch : "Supplies", categoryName : "لوازم هنری" , name : "خمیر شمع" , pricy : 100 , img : "imgs/New Products/Supplies Products/Product_1.png" },
+        { categorySearch : "Supplies", categoryName : "لوازم هنری" , name : "خمیر شمع" , pricy : 100 , img : "imgs/New Products/Supplies Products/Product_2.png" },
+        { categorySearch : "Supplies", categoryName : "لوازم هنری" , name : "خمیر شمع" , pricy : 100 , img : "imgs/New Products/Supplies Products/Product_3.png" },
+        { categorySearch : "Supplies", categoryName : "لوازم هنری" , name : "خمیر شمع" , pricy : 100 , img : "imgs/New Products/Supplies Products/Product_4.png" },
  
-        { categoryName : "Tools", name : "پالت" , pricy : 165 , img : "imgs/New Products/Tools Products/Product_1.png" },
-        { categoryName : "Tools", name : "قلمو" , pricy : 165 , img : "imgs/New Products/Tools Products/Product_2.png" },
-        { categoryName : "Tools", name : "قلمو" , pricy : 165 , img : "imgs/New Products/Tools Products/Product_3.png" },
-        { categoryName : "Tools", name : "قلمو" , pricy : 165 , img : "imgs/New Products/Tools Products/Product_4.png" },
+        { categorySearch : "Tools", categoryName : "ابزار و لوازم" , name : "پالت" , pricy : 165 , img : "imgs/New Products/Tools Products/Product_1.png" },
+        { categorySearch : "Tools", categoryName : "ابزار و لوازم" , name : "قلمو" , pricy : 165 , img : "imgs/New Products/Tools Products/Product_2.png" },
+        { categorySearch : "Tools", categoryName : "ابزار و لوازم" , name : "قلمو" , pricy : 165 , img : "imgs/New Products/Tools Products/Product_3.png" },
+        { categorySearch : "Tools", categoryName : "ابزار و لوازم" , name : "قلمو" , pricy : 165 , img : "imgs/New Products/Tools Products/Product_4.png" },
 ];
 
 /// start Chack Href Page ///
@@ -41,7 +41,7 @@ window.addEventListener("load",function(){
     let locationSearch = new URLSearchParams(location.search);
     let categoryName = locationSearch.get("Product");
     categorys.forEach(function(category){
-        if( categoryName == category.categoryName ){
+        if( categoryName == category.categorySearch ){
             appendProducts(category);
         }
     });
@@ -51,16 +51,26 @@ window.addEventListener("load",function(){
 
 /// start append products in html ///
 
-function appendProducts(category){
+function appendProducts(item){
 
-    NewProducts.insertAdjacentHTML("beforeend" , "<li><div><img class=\"ProductsImg\" src=\""+ category.img +"\" alt=\"ProductsImg\"><p class=\"ProductsName\">"+ category.name +"</p><p class=\"price\">"+ category.pricy +" تومان</p></div></li>");
-    bestSellerItem.insertAdjacentHTML("beforeend" , "<li><div><img class=\"ProductsImg\" src=\""+ category.img +"\" alt=\"ProductsImg\"><p class=\"ProductsName\">"+ category.name +"</p><p class=\"price\">"+ category.pricy +" تومان</p></div></li>");
-    otherItem.insertAdjacentHTML("beforeend" , "<li><div><img class=\"ProductsImg\" src=\""+ category.img +"\" alt=\"ProductsImg\"><p class=\"ProductsName\">"+ category.name +"</p><p class=\"price\">"+ category.pricy +" تومان</p></div></li>");
-    otherItem.insertAdjacentHTML("beforeend" , "<li><div><img class=\"ProductsImg\" src=\""+ category.img +"\" alt=\"ProductsImg\"><p class=\"ProductsName\">"+ category.name +"</p><p class=\"price\">"+ category.pricy +" تومان</p></div></li>");
+    NewProducts.insertAdjacentHTML("beforeend" , "<li><div id=\"item.categoryName\" onclick=\"goCart(event)\"><img class=\"ProductsImg\" src=\""+ item.img +"\" alt=\"ProductsImg\"><p class=\"ProductsName\">"+ item.name +"</p><p class=\"price\">"+ item.pricy +" تومان</p></div></li>");
+    bestSellerItem.insertAdjacentHTML("beforeend" , "<li><div id=\"item.categoryName\" onclick=\"goCart(event)\"><img class=\"ProductsImg\" src=\""+ item.img +"\" alt=\"ProductsImg\"><p class=\"ProductsName\">"+ item.name +"</p><p class=\"price\">"+ item.pricy +" تومان</p></div></li>");
+    otherItem.insertAdjacentHTML("beforeend" , "<li><div id=\"item.categoryName\" onclick=\"goCart(event)\"><img class=\"ProductsImg\" src=\""+ item.img +"\" alt=\"ProductsImg\"><p class=\"ProductsName\">"+ item.name +"</p><p class=\"price\">"+ item.pricy +" تومان</p></div></li>");
+    otherItem.insertAdjacentHTML("beforeend" , "<li><div id=\"item.categoryName\" onclick=\"goCart(event)\"><img class=\"ProductsImg\" src=\""+ item.img +"\" alt=\"ProductsImg\"><p class=\"ProductsName\">"+ item.name +"</p><p class=\"price\">"+ item.pricy +" تومان</p></div></li>");
 
 }
 
 /// end append products in html ///
+
+function goCart(event){
+    let parentTarget = event.target.parentElement;
+    let categoryTarget = parentTarget.id;
+    let imgTarget = parentTarget.firstChild.src;
+    let nameTarget = parentTarget.firstChild.nextElementSibling.innerHTML;
+    let pricyTarget = parentTarget.lastChild.innerHTML;
+    localStorage.setItem("productInfo",JSON.stringify({category :  categoryTarget , name : nameTarget , pricy : pricyTarget , img : imgTarget}));
+    location.href = "https://sajadanjiidanii.github.io/Purchase-page/";
+}
 
 /// start create slider ///
 const slider = $.getElementById("slider-img");
